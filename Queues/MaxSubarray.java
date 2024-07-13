65f6f5095e54f7424dde45b142d1ce1176cba74c
Abdul Rabb Nawaz <abdulrabb41@gmail.com>
2024-07-13 11:12:54 +0530

    arraydeque

---
+++ Queues/MaxSubarray.java
@@ -0,0 +1,47 @@
+package Queues;
+import java.util.*;
+
+public class MaxSubarray {
+
+    public static void printMax(int arr[], int k){
+        Deque<Integer> dq = new ArrayDeque<Integer>();
+
+        for(int i=0; i<k; i++){
+            while(!dq.isEmpty() && arr[i] >= arr[dq.peekLast()]){
+                dq.removeLast();
+            }
+            dq.addLast(i);
+        }
+
+        for(int i=k; i<arr.length; i++){
+            System.out.println(arr[dq.peek()] + " ");
+            while (!dq.isEmpty() && dq.peek() <= i-k) {
+                dq.removeFirst();
+            }
+            while (!dq.isEmpty() && arr[i] >= arr[dq.peekLast()]) {
+                dq.removeLast();
+            }
+            dq.addLast(i);
+        }
+        System.out.println(arr[dq.peek()] + " ");
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size of array: ");
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+
+        System.out.println("Enter k: ");
+        int k = sc.nextInt();
+
+        for(int i=0; i<arr.length; i++){
+            arr[i] = sc.nextInt();
+        }
+
+        printMax(arr, k);
+
+        sc.close();
+    }
+    
+}
